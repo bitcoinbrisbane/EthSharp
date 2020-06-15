cba59f9c18c9334bf7cf2a1146e86eae997a7311
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-06-15 16:17:22 +1000

    Begin evm

---
+++ EthSharp.Compiler/EVM.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EthSharp.Compiler
+{
+    public class EVM
+    {
+        
+    }
+}
